5563362f07cbf8a66b222644540b007df7cdc564
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-21 00:10:25 +0530

    DS and STL - STL-Array Done

--- DS-STL/STLArray.cpp
+++ DS-STL/STLArray.cpp
@@ -29,13 +29,25 @@ int main(){
 
     Print(arr);
     cout<<endl;
-    // for(int i =0; i < arr.size(); i++){
-    // cout<<arr[i]<<endl;
-    // }
+  
 
     sort(arr.begin(), arr.end());
+        Print(arr);
 
-    Print(arr);
+        cout<<endl;
+    // Fill - If we want an array with each element same
+    array<int,10> fives;
+    fives.fill(5);
+
+      for(int i =0; i < arr.size(); i++){
+    cout<<fives[i]<<" ";
+    }
+    cout<<endl;
+    // For Each Method
+
+    for(auto x:arr){
+        cout<<x<<" ";
+    }
 
     return 0;
 }
